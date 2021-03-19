bd977d037c0edbf9a001c23727dd87d4678b75ce
Kristen <kristenwood123@gmail.com>
2021-03-18 18:14:59 -0700

    fixed background color

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-let btn = document.querySelector('.darkmode')
-
-
-
+let btn = document.querySelector('.darkmode');
 
 btn.addEventListener('click', () => {
-  document.body.classList.add('darkmode')
   if(btn.innerHTML === `<i class="fas fa-moon"></i>`) {
     btn.innerHTML = `<i class="fas fa-sun"></i>`
-    document.body.classList.remove('darkmode')
+    document.body.classList.add('darkmode')
   } else {
     btn.innerHTML = `<i class="fas fa-moon"></i>`
+    document.body.classList.remove('darkmode')
   }
 })
