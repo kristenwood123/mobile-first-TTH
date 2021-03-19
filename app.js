8f55c782d4bd34549c72bc129e6633bdc40b9c99
Kristen <kristenwood123@gmail.com>
2021-03-18 18:07:55 -0700

    functionality of darktheme

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let btn = document.querySelector('.btn')
-let portfolio = document.querySelector('.portfolio')
-let wrapper = document.querySelector('.wrapper')
+let btn = document.querySelector('.darkmode')
+
 
 
 
 btn.addEventListener('click', () => {
   document.body.classList.add('darkmode')
+  if(btn.innerHTML === `<i class="fas fa-moon"></i>`) {
+    btn.innerHTML = `<i class="fas fa-sun"></i>`
+    document.body.classList.remove('darkmode')
+  } else {
+    btn.innerHTML = `<i class="fas fa-moon"></i>`
+  }
 })
-
