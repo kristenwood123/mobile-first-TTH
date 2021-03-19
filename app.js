e17fb98dea3992e2ed53444b9cf4008e2e0076a2
Kristen <kristenwood123@gmail.com>
2021-03-18 17:49:48 -0700

    added dark theme js

---
+++ app.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector('.btn')
+let portfolio = document.querySelector('.portfolio')
+let wrapper = document.querySelector('.wrapper')
+
+
+
+btn.addEventListener('click', () => {
+  document.body.classList.add('darkmode')
+})
+
